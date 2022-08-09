aba4ffb3dbc6baacb65a1b9fea94ae2bfcbe92cc
SimonT <43646872+SimonTil@users.noreply.github.com>
2022-08-09 22:36:22 +0200

    Test files Arduino

---
+++ Source code/Printer/Test files/Buttons/Buttons.ino
@@ -0,0 +1,19 @@
+// Prints 4 values: button 1, button 2, paper sensor, and enstop
+
+void setup(){
+    Serial.begin(9600);
+    pinMode(7, INPUT_PULLUP); // button 1
+    pinMode(8, INPUT_PULLUP); // button 2
+    pinMode(9, INPUT_PULLUP); // paper sensor
+    pinMode(3, INPUT_PULLUP); // endstop
+}
+
+void loop(){
+    Serial.print(digitalRead(7));
+    Serial.print(", ");
+    Serial.print(digitalRead(8));
+    Serial.print(", ");
+    Serial.print(digitalRead(9));
+    Serial.print(", ");
+    Serial.println(digitalRead(3));
+}
